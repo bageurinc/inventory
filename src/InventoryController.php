0a6fe025789c866f8317de1da09555ae06679385
Adnan Bayu <adnan.bayufirmansyah@gmail.com>
2020-11-17 13:31:02 +0700

    First Feature

---
+++ src/InventoryController.php
@@ -0,0 +1,127 @@
+<?php
+namespace Bageur\Inventory;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Bageur\Inventory\model\inventory;
+use Bageur\Inventory\Processors\UploadProcessor;
+use Validator;
+class InventoryController extends Controller
+{
+
+    public function index(Request $request)
+    {
+       $query = inventory::datatable($request);
+       return $query;
+    }
+
+    public function store(Request $request)
+    {
+        // return $request;
+        $rules    	= [
+                        'nama'                      => 'required|unique:bgr_inventory|min:3',
+                        'unit'                      => 'required',
+                        'jenis_produk'              => 'required',
+                        'qty'                       => 'required|numeric',
+                        'harga'                     => 'required|numeric',
+                    ];
+        if($request->file('gambar') != null){
+            $rules['gambar'] = 'mimes:jpg,jpeg,png,svg|max:1000';
+        }              
+        $messages 	= [];
+        $attributes = [];
+
+        $validator = Validator::make($request->all(), $rules,$messages,$attributes);
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response(['status' => false ,'error'    =>  $errors->all()], 200);
+        }else{
+
+            $inventory                          = new inventory;
+            $inventory->nama                    = $request->nama;
+            $inventory->nama_seo                = Str::slug($request->nama);
+            $inventory->keterangan              = $request->keterangan;
+            $inventory->jenis_produk            = $request->jenis_produk;
+            $inventory->unit                    = $request->unit;
+            $inventory->qty                     = $request->qty;
+            $inventory->harga                   = $request->harga;
+            if($request->file('gambar') != null){
+                $upload                     = UploadProcessor::go($request->file('gambar'),'inventory');
+                $inventory->gambar          = $upload;
+            }
+            
+            $inventory->save();
+            return response(['status' => true ,'text'    => 'has input'], 200); 
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return inventory::findOrFail($id);
+    }   
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $rules      = [
+                        'nama'                      => 'required|unique:bgr_inventory,nama,'.$id.',id|min:3',
+                        'unit'                      => 'required',
+                        'jenis_produk'              => 'required',
+                        'qty'                       => 'required|numeric',
+                        'harga'                     => 'required|numeric',
+                    ];
+        if($request->file('gambar') != null){
+            $rules['gambar'] = 'mimes:jpg,jpeg,png,svg|max:1000';
+        }              
+        $messages   = [];
+        $attributes = [];
+
+        $validator = Validator::make($request->all(), $rules,$messages,$attributes);
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response(['status' => false ,'error'    =>  $errors->all()], 200);
+        }else{
+            $inventory                          = inventory::findOrFail($id);
+            $inventory->nama                    = $request->nama;
+            $inventory->nama_seo                = Str::slug($request->nama);
+            $inventory->keterangan              = $request->keterangan;
+            $inventory->jenis_produk            = $request->jenis_produk;
+            $inventory->unit                    = $request->unit;
+            $inventory->qty                     = $request->qty;
+            $inventory->harga                   = $request->harga;
+            if($request->file('gambar') != null){
+                $upload                     = UploadProcessor::go($request->file('gambar'),'inventory');
+                $inventory->gambar          = $upload;
+            }
+            $inventory->save();
+            return response(['status' => true ,'text'    => 'has input'], 200); 
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $delete = inventory::findOrFail($id);
+        $delete->delete();
+        return response(['status' => true ,'text'    => 'has deleted'], 200); 
+    }
+
+}
---
+++ src/model/inventory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Bageur\Inventory\Model;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Bageur\Inventory\Processors\AvatarProcessor;
+
+class inventory extends Model
+{
+    protected $table   = 'bgr_inventory';
+    protected $appends = ['avatar', 'harga'];
+
+    public function getAvatarAttribute()
+    {
+            return AvatarProcessor::get($this->nama,@$this->gambar);
+    }     
+    public function getHargaAttribute()
+    {
+            $cur = number_format($this->double, 0, '.', '.');
+            return $cur;
+    }     
+    public function scopeDatatable($query,$request,$page=7)
+    {
+          $search       = ["nama"];
+          $searchqry    = '';
+
+        $searchqry = "(";
+        foreach ($search as $key => $value) {
+            if($key == 0){
+                $searchqry .= "lower($value) like '%".strtolower($request->search)."%'";
+            }else{
+                $searchqry .= "OR lower($value) like '%".strtolower($request->search)."%'";
+            }
+        } 
+        $searchqry .= ")";
+        if(@$request->sort_by){
+            if(@$request->sort_by != null){
+            	$explode = explode('.', $request->sort_by);
+                 $query->orderBy($explode[0],$explode[1]);
+            }else{
+                  $query->orderBy('created_at','desc');
+            }
+
+             $query->whereRaw($searchqry);
+        }else{
+             $query->whereRaw($searchqry);
+        }
+        if($request->get == 'all'){
+            return $query->get();
+        }else{
+                return $query->paginate($page);
+        }
+
+    }
+}
